07d993b861f6ebfc8510ca17181be1e32b37e4fd
Rick van Schalkwijk <rvanschalkwijk@mirabeau.nl>
2015-07-08 17:37:57 +0200

    added unit tests for IoC

---
+++ Source/Console.Reader.DependencyInjection.UnitTests/IoCUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using StructureMap;
+
+using IContainer = StructureMap.IContainer;
+
+namespace Console.Reader.DependencyInjection.UnitTests
+{
+    [TestFixture]
+    public class IoCUnitTests
+    {
+        [Test]
+        public void Initialize_ShouldInitIoC_ReturnBool()
+        {
+            IContainer container = new Container();
+            container.AssertConfigurationIsValid();
+        }
+    }
+}
