4965785264e4990a44cb6770d8a38fe1d0c77721
Rick van Schalkwijk <rvanschalkwijk@mirabeau.nl>
2015-07-09 13:17:43 +0200

    added yml file for travis ci

---
+++ Source/Console.Reader/ConsoleHost.cs
@@ -0,0 +1,19 @@
+﻿namespace Console.Reader
+{
+    public class ConsoleHost
+    {
+        public void Run()
+        {
+
+
+        }
+
+        private void InitConsoleWindow()
+        {
+            System.Console.SetWindowPosition(0, 0);
+            System.Console.Title = "Umbraco Sync Console";
+            System.Console.WindowWidth = 170;
+            System.Console.WindowHeight = 75;
+        }
+    }
+}
--- Source/Console.Reader/Program.cs
+++ Source/Console.Reader/Program.cs
@@ -13,7 +13,6 @@ namespace ConsoleReader
         {
             // init structure map
             IoC.Initialize();
-
         }
     }
 }
